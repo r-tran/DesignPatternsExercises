f0c37f18a4fe0fc7bf88d7c0c6324a648bc0cbea
Ray Tran <r8tran@gmail.com>
2017-06-11 11:08:05 -0700

    first commit

---
+++ DesignPatterns.Test/SingletonTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns.Creational.Singleton;
+
+namespace DesignPatterns.Test
+{
+    [TestClass]
+    public class SingletonTests
+    {
+        [TestMethod]
+        public void LazyDoubleCheckedProduction_Singleton_EnsuresOneInstance()
+        {
+            var president1 = LazyDoubleCheckedLockingProduction.President.Instance;
+            var president2 = LazyDoubleCheckedLockingProduction.President.Instance;
+            var president3 = LazyDoubleCheckedLockingProduction.President.Instance;
+
+            Assert.AreEqual(president1, president2);
+            Assert.AreEqual(president2, president3);
+        }
+
+        [TestMethod]
+        public void LazyImplementation_Singleton_EnsuresOneInstance()
+        {
+            var president1 = LazyImplementation.President.Instance;
+            var president2 = LazyImplementation.President.Instance;
+            var president3 = LazyImplementation.President.Instance;
+
+            Assert.AreEqual(president1, president2);
+            Assert.AreEqual(president2, president3);
+        }
+
+        [TestMethod]
+        public void LazyImplementationUsingSyncLock_Singleton_EnsuresOneInstance()
+        {
+            var president1 = LazyImplementationUsingSyncLock.President.Instance;
+            var president2 = LazyImplementationUsingSyncLock.President.Instance;
+            var president3 = LazyImplementationUsingSyncLock.President.Instance;
+
+            Assert.AreEqual(president1, president2);
+            Assert.AreEqual(president2, president3);
+        }
+
+        [TestMethod]
+        public void LazyImplementationUsingDoubleCheckedSyncLock_Singleton_EnsuresOneInstance()
+        {
+            var president1 = LazyImplementationUsingDoubleCheckedSyncLock.President.Instance;
+            var president2 = LazyImplementationUsingDoubleCheckedSyncLock.President.Instance;
+            var president3 = LazyImplementationUsingDoubleCheckedSyncLock.President.Instance;
+
+            Assert.AreEqual(president1, president2);
+            Assert.AreEqual(president2, president3);
+        }
+    }
+}
---
+++ DesignPatterns/Creational/Singleton/President.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    public class LazyDoubleCheckedLockingProduction
+    {
+        public class President
+        {
+            private readonly static Lazy<President> presidentInstance = new Lazy<President>(() => new President());
+            private President() { }
+
+            public static President Instance => presidentInstance.Value;
+        }
+    }
+
+    public class LazyImplementation
+    {
+        public class President
+        {
+            private static President presidentInstance;
+            private President() { }
+
+            public static President Instance
+            {
+                get
+                {
+                    if (presidentInstance == null)
+                        presidentInstance = new President();
+                    return presidentInstance;
+                }
+            }
+        }
+    }
+
+    public class LazyImplementationUsingSyncLock
+    {
+        public class President
+        {
+            private static President _presidentInstance;
+            private static readonly object _syncLock = new object();
+
+            private President() { }
+
+            public static President Instance
+            {
+                get
+                {
+                    lock (_syncLock)
+                    {
+                        if (_presidentInstance == null)
+                            _presidentInstance = new President();
+                    }
+
+                    return _presidentInstance;
+                }
+            }
+        }
+    }
+
+    public class LazyImplementationUsingDoubleCheckedSyncLock
+    {
+        public class President
+        {
+            private static President _presidentInstance;
+            private static readonly object _syncLock = new object();
+
+            private President() { }
+
+            public static President Instance
+            {
+                get
+                {
+                    if (_presidentInstance == null)
+                    {
+                        lock (_syncLock)
+                        {
+                            if (_presidentInstance == null)
+                                _presidentInstance = new President();
+                        }
+                    }
+
+                    return _presidentInstance;
+                }
+            }
+        }
+    }
+}
